587ccb2d2471d4c606a26eb36155c6b1f87d214c
yuanyl <542867428@qq.com>
2018-12-23 08:24:36 +0800

    提交 simple 分支

--- app/src/main/java/com/activity/IRecyclerView.java
+++
@@ -1,9 +0,0 @@
-package com.activity;
-
-/**
- * Created by lyf on 2016/6/17.
- */
-public interface IRecyclerView<T> {
-    BaseRecyclerAdapter<T> newAdapter();
-    ItemViewCreator<T> configItemViewCreator();
-}
